657a9837b8d6c650bd9153bb930c8c723199bd10
Troy Fox <blacksmith78@gmail.com>
2020-06-08 23:53:26 -0700

    Updates

---
+++ precache-manifest.0f810ffea45ff0a3339af6bf50d69979.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68c865372a5aa0ad00648fa755bc22a7",
+    "url": "/PortfoloCalculator/index.html"
+  },
+  {
+    "revision": "2867b3c1371871a2e87d",
+    "url": "/PortfoloCalculator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "140fdcd5007a4df9eedd",
+    "url": "/PortfoloCalculator/static/js/2.1fd2f2cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PortfoloCalculator/static/js/2.1fd2f2cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2867b3c1371871a2e87d",
+    "url": "/PortfoloCalculator/static/js/main.0e2b50ce.chunk.js"
+  },
+  {
+    "revision": "3c9f4fbe410b92f40599",
+    "url": "/PortfoloCalculator/static/js/runtime-main.1b657fea.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/PortfoloCalculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
