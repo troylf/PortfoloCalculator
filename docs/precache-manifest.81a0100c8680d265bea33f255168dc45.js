b92f4d7558328b56fe5c81a0afdf3051a92998d8
Troy Fox <blacksmith78@gmail.com>
2020-06-08 23:44:47 -0700

    compiled docs

---
+++ docs/precache-manifest.81a0100c8680d265bea33f255168dc45.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "218877cf72dfeb63fc0eb717eaa62739",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b0fae205fe94fe56f89",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "678c264870a903edcfd8",
+    "url": "/static/js/2.e5bd7815.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e5bd7815.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b0fae205fe94fe56f89",
+    "url": "/static/js/main.41f7334d.chunk.js"
+  },
+  {
+    "revision": "aa17fe5bc30a6a8cab39",
+    "url": "/static/js/runtime-main.e2aaa414.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
